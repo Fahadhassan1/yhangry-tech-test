3805ebc022b3f1c2c11bb7280dd7dc79c1d87783
Fahad Hassan <46788252+Fahadhassan1@users.noreply.github.com>
2025-01-08 20:41:43 +0000

    update the react file change the images

--- resources/js/Components/Home.jsx
+++ resources/js/Components/Home.jsx
@@ -4,8 +4,8 @@ import axios from 'axios';
 const Home = () => {
     const [setMenus, setSetMenus] = useState([]);
     const [cuisines, setCuisines] = useState([]);
-    const [cuisineSlug, setCuisineSlug] = useState('BBQ');
-    const [perPage, setPerPage] = useState(20);
+    const [cuisineSlug, setCuisineSlug] = useState('');
+    const [perPage, setPerPage] = useState(10);
     const [guests, setGuests] = useState(1);
     const [totalPrice, setTotalPrice] = useState(0);
     const [minSpend, setMinSpend] = useState(100);
@@ -16,6 +16,8 @@ const Home = () => {
 
     const fetchSetMenus = async () => {
         try {
+
+            
             const response = await axios.get(`http://127.0.0.1:8000/api/set-menus`, {
                 params: {
                     cuisineSlug,
@@ -44,39 +46,50 @@ const Home = () => {
 
     return (
         <div>
-            <h1>Set Menus</h1>
-            <div>
+            <h1 className='text-center mt-3'>Set Menus</h1>
+            <div className='text-center'>
                 <label>
                     Number of Guests:
                     <input type="number" value={guests} onChange={handleGuestsChange} />
                 </label>
-                <p>Total Price: ${totalPrice}</p>
+            
             </div>
-            <div>
+            <div className='text-center mt-3'>
                 <label>
                     Cuisine:
                     <select value={cuisineSlug} onChange={(e) => setCuisineSlug(e.target.value)}>
+                        <option value="">ALL </option>
                         {cuisines.map((cuisine) => (
-                            <option key={cuisine.slug} value={cuisine.slug}>
+                            <option key={cuisine.id} value={cuisine.slug}>
                                 {cuisine.name}
                             </option>
                         ))}
                     </select>
                 </label>
             </div>
-            <div>
-                <ul>
+            <div className='text-center mt-3'> <p>Total Price: ${totalPrice}</p></div>
+            <div className='container'>
+                <div className='mt-4 d-flex flex-wrap justify-content-between'>
+
+                  <div className='col-md-12 row ms-5'>
                     {Array.isArray(setMenus) && setMenus.map((menu) => (
-                        <li key={menu.id}>
-                        <img src={menu.thumbnail} alt={menu.name} style={{ width: '100px', height: '100px' }} />
-                        <div>
-                            {menu.name} - ${menu.price_per_person}
+                        <div className='col-md-4' key={menu.id}>
+                                <img src={menu.thumbnail} alt={menu.name} style={{ width: '100px', height: '100px' }} />
+                                <div>
+                                    {menu.name} - ${menu.price_per_person}
+                                </div>
                         </div>
-                    </li>
-                    ))}
-                </ul>
-                <button onClick={() => setPerPage(perPage + 20)}>Show more</button>
+                        ))}
+                    </div>
+                </div>
+
+
+                <div className='text-center mt-4'>            
+                <button className='text-center mt-4' onClick={() => setPerPage(perPage + 20)}>Show more</button>
+                </div>
+
             </div>
+
         </div>
     );
 };
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
+import '../css/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JS
 
 createInertiaApp({
-    resolve: name => import(`./Pages/${name}`),
+    resolve: name => import(`./Components/${name}`),
     setup({ el, App, props }) {
         const root = createRoot(el);
         root.render(<App {...props} />);
